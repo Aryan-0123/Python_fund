9602b897a29f6509a3fe355459dd9d3c26f7f8be
Aryan Singh <tharyan284@gmail.com>
2026-08-23 16:28:50 +0530

    Initial commit

---
+++ day1.py
@@ -0,0 +1,9 @@
+#working with list tuples and dictionaries
+my_list = [1, 2, 3]
+print(len(my_list))
+my_list.append(4)
+print(my_list)
+my_list.insert(1, 1.5)
+print(my_list)
+print(my_list.pop())
+print(my_list)
